a47b4d2679a9800c7b389d7aa033303733f3e1cc
kralovcova <jirina.kralovcova@tul.cz>
2021-05-04 10:22:42 +0200

    Kralovcova - rekurze

---
+++ src/kralovcova/p071/rekurze/Permutace.java
@@ -0,0 +1,30 @@
+package kralovcova.p071.rekurze;
+
+public class Permutace {
+
+    public static void ntice(int[] a, int n) {
+        if (n <= 0) {
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                boolean je = false;
+                for (int j = a.length - 1; j >= n; j--) {
+                    if (a[j] == i) {
+                        je = true;
+                    }
+                }
+                if (!je) {
+                    a[n - 1] = i;
+                    ntice(a, n - 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ntice(new int[5], 5);
+    }
+}
---
+++ src/kralovcova/p071/rekurze/ProchazeniAdresarovehoStromu.java
@@ -0,0 +1,49 @@
+/*
+ * This code is part of ALP subject
+ */
+
+package kralovcova.p071.rekurze;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jirina
+ */
+public class ProchazeniAdresarovehoStromu {
+
+    public static long spaceUsed(File path) {
+        if (path.isFile()) {
+            return path.length();
+        } else if (path.isDirectory()) {
+            File[] ff = path.listFiles();
+            if (ff == null) return 0;
+            long v = 0;
+            for (File f : ff) {
+                v = v + spaceUsed(f);
+            }
+            return v;
+        }
+        System.out.println("Blind branch");
+        return 0;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Velikost adresare na disku");
+        System.out.println("Jak velky prostor v bytech zabira dany soubor nebo adresar (vcetne podadresaru)?");
+        System.out.println("Zadej cestu k soboru nebo adresari");
+        String pathStr = sc.nextLine();
+        File pathFile = (new File(pathStr)).getCanonicalFile();
+        long velNaDisku = spaceUsed(pathFile);
+        System.out.format("Zadany soubor/adresar: %s%n", pathFile.getName());
+        System.out.format("Plna cesta k souboru/adresari: %s%n", pathFile.getAbsolutePath());
+        System.out.format("Data v zadanem adresari zabiraji %,d byte%n", velNaDisku);
+    }
+
+}
